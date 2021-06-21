7475643e47f63c6965ba31ff54a22e277bea8826
sanchodev25 <sanchodev25@gmail.com>
2021-06-20 23:10:37 -0700

    On the app.js file I set up the methods for getting the user inputs and also made a resetValue function. On the index.html file I was usingit to trouble shoot, but everything in the file stayed the same

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-//hello
-//this the new code that I write
-//another line of code
+const submitBtn = document.querySelector("#submit");
+const resetBtn = document.querySelector("#reset_val");
+
+//reset, send, validate
+const resetValues = (e) => {
+  e.preventDefault();
+  document.querySelector("#first_name").value = "";
+  document.querySelector("#pay_rate").value = "0.00";
+  document.querySelector("#hours").value = "0.00";
+  document.querySelector("#overtime").value = "0.00";
+};
+
+const getValues = (e) => {
+  e.preventDefault();
+  let hourlyPayRate = document.querySelector("#pay_rate").value;
+  let hoursWorked = document.querySelector("#hours").value;
+  let hoursOvertime = document.querySelector("#overtime").value;
+  console.log(hourlyPayRate);
+};
+
+submitBtn.addEventListener("click", getValues);
+resetBtn.addEventListener("click", resetValues);
